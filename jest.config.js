860b6d8c6b2b881b89079beacc581c0d9142d189
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2022-05-14 10:19:47 -0300

    README

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exposts = {
+    "moduleFileExtensions": [
+      "js",
+      "json",
+      "ts"
+    ],
+    "rootDir": "src",
+    "testRegex": ".*\\.spec\\.ts$",
+    "transform": {
+      "^.+\\.(t|j)s$": "ts-jest"
+    },
+    "collectCoverageFrom": [
+      "**/*.(t|j)s"
+    ],
+    "coverageDirectory": "../coverage",
+    "testEnvironment": "node"
+}
